4526e19b18dba6e0d76df052d21d0906832f51bc
Dylan Higgins <higgindy@mail.uc.edu>
2023-11-12 13:47:19 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        if (age >= 21)
+        {
+            System.out.println("Congratulations! You get a wristband.");
+        }
+    }
+}
